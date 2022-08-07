51cb485d51f02bed41736aca2ae1a74592410b0f
fatimakhatungit <fatimakhatuncse@gmail.com>
2022-08-07 16:32:32 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, {useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
-  const [countries, setCountries] = useState([]);
+   const [countries, setCountries] = useState([]);
+   const [cart, setCart] = useState([]);
 
-
-  useEffect(() =>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data=>{
-      setCountries(data);
-      console.log(data); 
-      const names = data.map(country => country.name)
-      console.log(names);   
-    }) 
-  }, []) 
- 
+   useEffect(()=>{
+     fetch('https://restcountries.eu/rest/v2/all')
+     .then(res => res.json())
+     .then(data=>setCountries(data))
+     .catch(error => console.log(error))
+   }, [])
+   
+   const handleAddCountry = (country) => {
+     const newCart = [...cart, country];
+     setCart(newCart);
+    }
 
   return (
     <div className="App">
-  <h1>countries loaded: {countries.length}</h1> 
-  <ul>
-   
+      <h1>country loaded: {countries.length}</h1>
+      <h4>Country added: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
       {
-        countries.map(country => <Country name={country.name}></Country>)   
-      } 
-  </ul>
+        countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+      }
+      
     
-    </div> 
+    </div>
   );
 }
 
